8ed1607cf3f76b072f16bc27b1892cb3f40bfa3b
matteocaschetto <matteocaschetto9@gmail.com>
2025-01-13 17:36:24 +0100

    U4-W2-D1-1COMMIT

--- src/ES2/CalcoloKmLitro.java
+++ src/ES2/CalcoloKmLitro.java
@@ -1,23 +1,24 @@
 package ES2;
 
 import java.util.Scanner;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 
 public class CalcoloKmLitro {
 
-    private static final Logger logger = LoggerFactory.getLogger(CalcoloKmLitro.class);
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         
         System.out.print("Inserisci il numero di km percorsi: ");
-        double km = scanner.nextDouble();
+         int km = Integer.parseInt(scanner.nextLine());
+
+
 
         
         System.out.print("Inserisci i litri di carburante consumati: ");
-        double litri = scanner.nextDouble();
+        int litri = Integer.parseInt(scanner.nextLine());
 
         try {
             
@@ -25,23 +26,20 @@ public class CalcoloKmLitro {
                 throw new ArithmeticException("Errore: Divisione per zero, i litri non possono essere zero.");
             }
 
-            double kmPerLitro = km / litri;
+            double kmPerLitro = (double) km / litri;
 
             
             System.out.println("Il consumo è di " + kmPerLitro + " km/litro.");
 
         } catch (ArithmeticException e) {
             
-            logger.error(e.getMessage());
             System.out.println("Errore: " + e.getMessage());
         } catch (Exception e) {
             
-            logger.error("Errore imprevisto: " + e.getMessage());
             System.out.println("Errore imprevisto: " + e.getMessage());
         } finally {
            
             scanner.close();
-            logger.info("Programma terminato.");
         }
     }
 }
